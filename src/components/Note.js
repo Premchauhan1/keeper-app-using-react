eaf6a24486e3054e304723bc31929fdff77aa54b
Prem Chauhan <tprem2733@gmail.com>
2024-02-13 21:35:13 +0530

    hosting page

--- src/components/Note.js
+++ src/components/Note.js
@@ -7,7 +7,8 @@ function Note(props){
     return <div className="note">
         <h1>{props.title}</h1>
         <p>{props.content}</p>
-        <button onClick={handleClick}><DeleteIcon / ></button>
+        <button onClick={handleClick}><DeleteIcon /
+        ></button>
     </div>
 }
 export default Note;
